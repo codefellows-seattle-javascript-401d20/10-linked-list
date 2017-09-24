ba79f776e55bb14193ab0ea13d2c76459e1ad3c0
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-24 15:13:51 -0700

    single to doubly

---
+++ lab-brandon/model/doubly-linked-list.js
@@ -0,0 +1,38 @@
+'use strict';
+
+class DoublyLinkedList {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+
+  append(node){
+    if(!(node instanceof DoublyLinkedList))
+      throw new Error('USAGE ERROR: node must be a Doubly Linked List');
+    if(!this.next)
+      this.next = node;
+    else
+    this.next.append(node);
+    this.next.next = node;
+
+    return this;
+  }
+
+  remove(node){
+    if(!(node instanceof DoublyLinkedList))
+      throw new Error('USAGE ERROR: node must be a Doubly Linked List');
+
+    if(this.next === null){
+      return this;
+    } else if (this.next === node){
+      this.next = this.next.next;
+    } else {
+      this.next.remove(node);
+    }
+
+    return this;
+  }
+}
+
+module.exports = DoublyLinkedList;
