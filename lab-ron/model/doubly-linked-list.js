d5533663780e7071fe932322d1baa12ff0707ea9
Ron Barrantes <ronbarrantes@gmail.com>
2017-09-24 15:34:28 -0700

    implementing the singly linked list

---
+++ lab-ron/model/doubly-linked-list.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+
+  append(node) {
+    if (!(node instanceof DoublyLinkedList))
+      throw new Error('__USAGE_ERROR__: Node must be a DoublyLinkedList');
+    if (!this.next)
+      this.next = node;
+    else
+      this.next.append(node);
+    return this;
+  }
+
+  remove(node) {
+    if (!(node instanceof DoublyLinkedList))
+      throw new Error('__USAGE_ERROR__: Node must be a DoublyLinkedList');
+
+    if (!this.next) {
+      return this;
+    } else if (this.next === node) {
+      this.next = this.next.next;
+    } else {
+      this.next.remove(node);
+    }
+    return this;
+  }
+}
+
+
+module.exports = DoublyLinkedList;
